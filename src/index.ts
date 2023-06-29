7cb6a3a3d9a1d9d66bc7ffa29c5a07da6a1d5b08
jiangr <jiangrui@rhtect.com>
2023-06-29 11:28:19 +0800

    TypeScript button事件测试

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,11 @@ import "./style/index.less"
 
 const gameControl = new GrameControl();
 gameControl.init()
+
+
+const btn = document.getElementById('start') as HTMLButtonElement
+//typescript为按钮绑定点击事件
+
+btn.addEventListener('click', () => {
+    console.log(111)
+})
